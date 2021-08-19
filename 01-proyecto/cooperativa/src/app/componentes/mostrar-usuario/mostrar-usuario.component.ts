e1b7e08fef467c6daf61da0d592f8ced319fc586
Adrian Eguez <eadepto@hotmail.com>
2021-08-18 20:45:20 -0500

    Clase input

--- 01-proyecto/cooperativa/src/app/componentes/mostrar-usuario/mostrar-usuario.component.ts
+++ 01-proyecto/cooperativa/src/app/componentes/mostrar-usuario/mostrar-usuario.component.ts
@@ -1,7 +1,7 @@
-import { Component, OnDestroy, OnInit } from '@angular/core';
+import { Component, Input, OnDestroy, OnInit } from '@angular/core';
 
 @Component({
-  selector: 'app-mostrar-usuario', // <app-mostrar-usuario></app-mostrar-usuario>
+  selector: 'app-mostrar-usuario', // <app-mostrar-usuario [usuario]="usuario"></app-mostrar-usuario>
   templateUrl: './mostrar-usuario.component.html',
   styleUrls: ['./mostrar-usuario.component.scss']
 })
@@ -13,6 +13,13 @@ export class MostrarUsuarioComponent implements OnInit, OnDestroy {
   objeto = {nombre:'Vicente'};
   arreglo = [1, 2, 3, 4];
 
+  @Input()
+  usuario = {
+    nombre: 'Adrian',
+    imagen: 'https://www.adslzone.net/app/uploads-adslzone.net/2019/04/borrar-fondo-imagen.jpg',
+    descripcion: 'Figura de una persona o cosa captada por el ojo, por un espejo, un aparato',
+  }
+
   constructor() { 
     // NO PONEMOS LOGICA
   }
@@ -21,6 +28,21 @@ export class MostrarUsuarioComponent implements OnInit, OnDestroy {
     // Inicializacion de la ruta EJ: llamamos a los datos iniciales
     console.log('OnInit');
   }
+
+  cambiarImagen(
+    evento: any,
+    usuario: any,
+    ){
+    console.log(evento);
+    console.log(usuario);
+
+    if(usuario.imagen === 'https://www.adslzone.net/app/uploads-adslzone.net/2019/04/borrar-fondo-imagen.jpg'){
+      usuario.imagen = 'https://3.bp.blogspot.com/-JfL1o7oSnKI/VmodObHF9cI/AAAAAAAABLY/nKKRXw0-yiU/s1600/homero_456_336.jpg';
+    }else{
+      usuario.imagen = 'https://www.adslzone.net/app/uploads-adslzone.net/2019/04/borrar-fondo-imagen.jpg';
+    }
+  }
+
 
   ngOnDestroy(): void {
     // Limpiar variables
--- 01-proyecto/cooperativa/src/app/rutas/ruta-login/ruta-login.component.ts
+++ 01-proyecto/cooperativa/src/app/rutas/ruta-login/ruta-login.component.ts
@@ -7,6 +7,26 @@ import { Component, OnInit } from '@angular/core';
 })
 export class RutaLoginComponent implements OnInit {
 
+  mostrarPrimerComponente = true;
+
+  usuarios = [
+    {
+      nombre: 'Adrian',
+      imagen: 'https://www.adslzone.net/app/uploads-adslzone.net/2019/04/borrar-fondo-imagen.jpg',
+      descripcion: 'Descripcion 1',
+    },
+    {
+      nombre: 'Vicente',
+      imagen: 'https://3.bp.blogspot.com/-JfL1o7oSnKI/VmodObHF9cI/AAAAAAAABLY/nKKRXw0-yiU/s1600/homero_456_336.jpg',
+      descripcion: 'Descripcion 2',
+    },
+    {
+      nombre: 'Carolina',
+      imagen: 'https://3.bp.blogspot.com/-JfL1o7oSnKI/VmodObHF9cI/AAAAAAAABLY/nKKRXw0-yiU/s1600/homero_456_336.jpg',
+      descripcion: 'Descripcion 3',
+    }
+  ]
+
   constructor() { }
 
   ngOnInit(): void {
